ff9875c524966c8ebbbd0bca0c948f7106920546
angelachan64 <angelachan64@gmail.com>
2015-10-22 13:04:23 -0400

    Create mymod.py

---
+++ SoftDev/mymod.py
@@ -0,0 +1,19 @@
+import random
+
+def get_rand(lower,upper):
+  return random.randrange(lower,upper);
+  
+print "number", get_rand(0,10)
+
+# any python program is a python module
+
+print "__name__",__name__
+# when this is run: _name__ __main__
+# when import as mod:__name__mymod
+
+# HTML is a markup
+# pyton virtualenv
+# install package
+# virtualenv pd7env
+# - creates a new directory with entire python installation
+# to use: go in bin, source activate
