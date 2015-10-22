f8264c30d28762c6d0d429e468f7027e6b1eb2d0
angelachan64 <angelachan64@gmail.com>
2015-10-22 12:49:33 -0400

    mongo.py

--- SoftDev/mongo.py
+++ SoftDev/mongo.py
@@ -4,6 +4,10 @@ from pymongo import Mongo Client
 # connection = MongoClient("hostname")
 connection = MongoClient();
 
+# for authentication
+# db = connection.admin
+# db.authenticate("username", "password")
+
 # we could also have done: db = connection.pd7
 db = connection['pd7']
 
