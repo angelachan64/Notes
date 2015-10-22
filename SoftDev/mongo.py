0d6c190d266cc04dd8c5074f50799df7357002f5
angelachan64 <angelachan64@gmail.com>
2015-10-22 12:53:41 -0400

    mongo.py

--- SoftDev/mongo.py
+++ SoftDev/mongo.py
@@ -1,5 +1,5 @@
 import random
-from pymongo import Mongo Client
+from pymongo import MongoClient
 
 # connection = MongoClient("hostname")
 connection = MongoClient();
@@ -8,7 +8,18 @@ connection = MongoClient();
 # db = connection.admin
 # db.authenticate("username", "password")
 
+# from command line: use pd7
 # we could also have done: db = connection.pd7
 db = connection['pd7']
 
+# from command line: show collections
 print db.collection_names()
+
+names = ['thluffy', 'bucky', 'sue', 'arnold', 'alice']
+dtypes = ['plain','jelly', 'glazed', 'frosted']
+
+for i in range(10):
+    d = {'name' : random.choice(names),
+         'donut' : random.choice(dtypes),
+         'age' : random.randrange(15,30)}
+    db.people.insert(d)
