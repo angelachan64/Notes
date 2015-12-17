d7fb0f8d8c763c5560beff39292fa406f6645931
angelachan64 <angelachan64@gmail.com>
2015-12-17 14:05:19 -0500

    Client side of pserver_basic.c

--- Systems/pclient-basic.c
+++ Systems/pclient-basic.c
@@ -6,6 +6,27 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+int client_handshake( int *from_server ){
+  
+  int to_server;
+  char buffer[100];
+  
+  sprintf( buffer, "%d", getpid() );
+  mkfifo( buffer, 0644 );
+  
+  to_server = open( "mario", O_WRONLY );
+  write( to_server, buffer, sizeof(buffer) );
+  
+  *from_server = open( buffer, O_RDONLY );
+  remove( buffer );
+  
+  read( *from_server, buffer, sizeof(buffer) );
+  printf("<client> connection established: [%s]\n", buffer);
+  
+  return to_server;
+  
+}
+
 int main() {
 
   int to_server;
@@ -13,5 +34,16 @@ int main() {
   char buffer[100];
   
   to_server = client_handshake( &from_server );
+  
+  while (1){
+    printf("<client> enter stuff: ");
+    fgets( buffer, sizeof(buffer), stdin );
+    write( to_server, buffer, sizeof(buffer) );
+    read( from_server, buffer, sizeof(buffer) );
+    printf( "<client> received: [%s]\n", buffer );
+  }
+  
+  close( to_server );
+  close( from_server );
 
 }
